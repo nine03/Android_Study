6df919f0fc25fd6188d888910acc5b03893d5cfb
Yeongjun Kim <60682087+nine03@users.noreply.github.com>
2022-03-31 00:13:23 +0900

    Add files via upload

---
+++ example/chapter04/exam0420/app/src/main/java/com/example/exam04_20/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.exam04_20;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView textResult;
+    String num1, num2;
+    Integer result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
