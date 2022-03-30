b3f2c3bbaad81941fb376807b5566059591d976a
Yeongjun Kim <60682087+nine03@users.noreply.github.com>
2022-03-31 00:41:03 +0900

    Add files via upload

---
+++ example/chapter04/exam0421/app/src/main/java/com/example/exam04_21/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.exam04_21;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView textResult;
+    String num1, num2;
+    Integer result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("초간단 계산기");
+
+        edit1 = (EditText) findViewById(R.id.Edit1);
+        edit2 = (EditText) findViewById(R.id.Edit2);
+        btnAdd = (Button) findViewById(R.id.BtnAdd);
+        textResult = (TextView) findViewById(R.id.TextResult);
+    }
+}
