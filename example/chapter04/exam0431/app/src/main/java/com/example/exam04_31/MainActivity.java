99bb653a26c83f04b38c6ad70ed04ae73aa8286a
Yeongjun Kim <60682087+nine03@users.noreply.github.com>
2022-04-02 11:59:37 +0900

    Add files via upload

---
+++ example/chapter04/exam0431/app/src/main/java/com/example/exam04_31/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.exam04_31;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text1, text2;
+    CheckBox chkAgree;
+    RadioGroup rGroup1;
+    RadioButton rdoDog, rdoCat, rdoRabbit;
+    Button btnOK;
+    ImageView imgPet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        setTitle("애완동물 사진 보기");
+
+        text1 = (TextView) findViewById(R.id.Text1);
+        chkAgree = (CheckBox) findViewById(R.id.ChkAgree);
+
+        text2 = (TextView) findViewById(R.id.Text2);
+        rGroup1 = (RadioGroup) findViewById(R.id.Rgroup1);
+        rdoDog = (RadioButton) findViewById(R.id.RdoDog);
+        rdoCat = (RadioButton) findViewById(R.id.RdoCat);
+        rdoRabbit = (RadioButton) findViewById(R.id.RdoRabbit);
+
+        btnOK = (Button) findViewById(R.id.BtnOK);
+        imgPet = (ImageView) findViewById(R.id.ImgPet);
+
+        chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(chkAgree.isChecked() == true) {
+                    text2.setVisibility( View.VISIBLE);
+                    rGroup1.setVisibility( View.VISIBLE);
+                    btnOK.setVisibility( View.VISIBLE);
+                    imgPet.setVisibility( View.VISIBLE);
+                } else {
+                    text2.setVisibility( View.INVISIBLE);
+                    rGroup1.setVisibility( View.INVISIBLE);
+                    btnOK.setVisibility( View.INVISIBLE);
+                    imgPet.setVisibility( View.INVISIBLE);
+                }
+            }
+        });
+    }
+}
