6e1f8706bcf61fdd546e3fa049a3f2ec11beba8c
Yeongjun Kim <60682087+nine03@users.noreply.github.com>
2022-04-08 02:03:35 +0900

    Add files via upload

---
+++ example/chapter05/exam0515/app/src/main/java/com/example/exam05_15/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.exam05_15;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
+    Button btnAdd, btnSub, btnMul, btnDiv;
+    TextView textResult;
+    String num1, num2;
+    Integer result;
+
+    // 10개 숫자 버튼 배열
+    Button[] numButtons = new Button[10];
+    // 10개 숫자 버튼의 id 값 배열
+    Integer[] numBtnIDs = { R.id.BtnNum0, R.id.BtnNum1, R.id.BtnNum2,
+            R.id.BtnNum3, R.id.BtnNum4, R.id.BtnNum5, R.id.BtnNum6,
+            R.id.BtnNum7, R.id.BtnNum8, R.id.BtnNum9 };
+    int i; // 증가값 용도
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
